bd4449324ec569cf585d53f8fc3547915962061a
Alex Blount <alexlblount@gmail.com>
2024-10-15 23:47:43 -0500

    audited migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_16_042746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "audits", force: :cascade do |t|
+    t.bigint "auditable_id"
+    t.string "auditable_type"
+    t.bigint "associated_id"
+    t.string "associated_type"
+    t.bigint "user_id"
+    t.string "user_type"
+    t.string "username"
+    t.string "action"
+    t.jsonb "audited_changes"
+    t.integer "version", default: 0
+    t.string "comment"
+    t.string "remote_address"
+    t.string "request_uuid"
+    t.datetime "created_at"
+  end
+
+  add_index "audits", ["associated_type", "associated_id"], name: "associated_index"
+
+  add_index "audits", ["auditable_type", "auditable_id", "version"], name: "auditable_index"
+
+  add_index "audits", ["audited_changes"], name: "audited_changes", using: :gin
+
+  add_index "audits", ["created_at"], name: "index_audits_on_created_at"
+
+  add_index "audits", ["request_uuid"], name: "index_audits_on_request_uuid"
+
+  add_index "audits", ["user_id", "user_type"], name: "user_index"
 
 end
